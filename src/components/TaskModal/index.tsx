a081264c43c75331c85537db346cba84fc498ea2
WagnerReis <wagnerreis1997@gmail.com>
2023-09-29 22:37:46 -0300

    feat: added ariaHideApp in modal

--- src/components/TaskModal/index.tsx
+++ src/components/TaskModal/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { IoMdClose } from "react-icons/io";
 import Modal from "react-modal";
-import styles from './styles.module.scss'
-import { useForm, SubmitHandler } from "react-hook-form"
+import styles from "./styles.module.scss";
+import { useForm, SubmitHandler } from "react-hook-form";
 import { api, getConfig } from "@/api";
 
 type Inputs = {
@@ -11,7 +11,7 @@ type Inputs = {
   estimated?: number;
   dueDate?: string;
   status: string;
-}
+};
 
 export function TaskModal({ isOpen, onClose, cards, setCards }: any) {
   const {
@@ -19,21 +19,21 @@ export function TaskModal({ isOpen, onClose, cards, setCards }: any) {
     handleSubmit,
     reset,
     formState: { errors },
-  } = useForm<Inputs>()
+  } = useForm<Inputs>();
   const onSubmit: SubmitHandler<Inputs> = (data) => {
-    data.status = "new"
-    api.post('/cards', data, getConfig())
-      .then(response => {
-        setCards([...cards, response.data])
-        onClose()
-        reset()
-      })
-  }  
+    data.status = "new";
+    api.post("/cards", data, getConfig()).then((response) => {
+      setCards([...cards, response.data]);
+      onClose();
+      reset();
+    });
+  };
 
   return (
     <Modal
       isOpen={isOpen}
       onRequestClose={onClose}
+      ariaHideApp={false}
       style={{
         overlay: {
           backgroundColor: "rgba(0, 0, 0, 0.5)",
@@ -48,73 +48,65 @@ export function TaskModal({ isOpen, onClose, cards, setCards }: any) {
       }}
     >
       <div className={styles.modalContent}>
-          <header className={styles.headerTask}>
-            <h2>Create Task</h2>
-            <IoMdClose
-              size={24}
-              className={styles.close}
-              onClick={onClose}
+        <header className={styles.headerTask}>
+          <h2>Create Task</h2>
+          <IoMdClose size={24} className={styles.close} onClick={onClose} />
+        </header>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className={styles.formGroup}>
+            <label className={styles.label} html-for="title">
+              Title:
+            </label>
+            <input
+              className={styles.inputText}
+              type="text"
+              id="title"
+              {...register("title", { required: true })}
             />
-          </header>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <div className={styles.formGroup}>
-              <label className={styles.label} html-for="title">
-                Title:
-              </label>
-              <input
-                className={styles.inputText}
-                type="text"
-                id="title"
-                {...register("title", { required: true })}
-              />
-              {errors.title && <span>This field is required</span>}
-            </div>
-            <div>
-              <div className={styles.formGroupTwo}>
-                <div>
-                  <label className={styles.label} html-for="description">
-                    Description:
-                  </label>
-                  <textarea
-                    className={styles.textarea}
-                    id="description"
-                    {...register("description")}
-                  ></textarea>
-                </div>
+            {errors.title && <span>This field is required</span>}
+          </div>
+          <div>
+            <div className={styles.formGroupTwo}>
+              <div>
+                <label className={styles.label} html-for="description">
+                  Description:
+                </label>
+                <textarea
+                  className={styles.textarea}
+                  id="description"
+                  {...register("description")}
+                ></textarea>
+              </div>
 
-                <div className={styles.estimated}>
-                  <div className={styles.formGroupThree}>
-                    <label className={styles.label} html-for="estimated">
-                      Estimate:
-                    </label>
-                    <input
-                      className={styles.inputNumber}
-                      type="number"
-                      id="estimated"
-                      {...register("estimated", { min: 0, max: 99 })}
-                      />
-                  </div>
-                  <div className={styles.formGroup}>
-                    <label className={styles.label} html-for="due-date">
-                      Due Date:
-                    </label>
-                    <input
-                      className={styles.inputDate}
-                      type="date"
-                      id="due-date"
-                      {...register("dueDate")}
-                    />
-                  </div>
+              <div className={styles.estimated}>
+                <div className={styles.formGroupThree}>
+                  <label className={styles.label} html-for="estimated">
+                    Estimate:
+                  </label>
+                  <input
+                    className={styles.inputNumber}
+                    type="number"
+                    id="estimated"
+                    {...register("estimated", { min: 0, max: 99 })}
+                  />
+                </div>
+                <div className={styles.formGroup}>
+                  <label className={styles.label} html-for="due-date">
+                    Due Date:
+                  </label>
+                  <input
+                    className={styles.inputDate}
+                    type="date"
+                    id="due-date"
+                    {...register("dueDate")}
+                  />
                 </div>
               </div>
             </div>
-            <input
-              className={styles.button}
-              type="submit"
-              value="Create"
-            />
-          </form>
-        </div>
+          </div>
+          <input className={styles.button} type="submit" value="Create" />
+        </form>
+      </div>
     </Modal>
   );
 }
